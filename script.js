ff9a7acfdd1768b4ba2449b4ac66141015070ca0
Harald Schilly <hsy@sagemath.com>
2019-08-02 14:47:55 +0000

    comms back + forth

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 console.log("js loaded");
 $(document).ready(function() {
   console.log("document ready");
-  $("button.open").on("click", function(e) {
+
+  function replies(mesg) {
+    if (
+      !(
+        mesg.origin.endsWith(".cocalc.com") ||
+        mesg.origin == "https://cocalc.com"
+      )
+    )
+      return;
+    $("#answer").html(JSON.stringify(event.data));
+  }
+
+  window.addEventListener("message", replies, false);
+
+  $("button#open").on("click", function(e) {
     e.preventDefault();
     console.log("sending open message");
     payload = {
       action: "open",
       project_id: "bc6f81b3-25ad-4d58-ae4a-65649fae4fa5",
       path: "openme.md"
+    };
+    $("#cocalc")
+      .get(0)
+      .contentWindow.postMessage(payload, "https://cocalc.com");
+    return false;
+  });
+
+  $("button#status").on("click", function(e) {
+    e.preventDefault();
+    console.log("sending open message");
+    payload = {
+      action: "status"
     };
     $("#cocalc")
       .get(0)
