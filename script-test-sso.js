797373c3d44bb7b52d0bc1119a1d6a064668b3c8
Harald Schilly <hsy@sagemath.com>
2019-11-12 15:37:50 +0000

    tweak script regarding different sso domain check

---
+++ script-test-sso.js
@@ -0,0 +1,76 @@
+$(document).ready(init);
+
+function init() {
+  console.log("initializing");
+
+  const frame = $("#cocalc").get(0).contentWindow;
+  const target = "https://cocalc-test.schil.ly";
+  const proj1 = "20e4a191-73ea-4921-80e9-0a5d792fc511";
+  const proj2 = "4a5f0542-5873-4eed-a85c-a18c706e8bcd";
+
+  function replies(mesg) {
+    const allowed =
+      mesg.origin.endsWith("cocalc-test.schil.ly") || mesg.origin == target;
+    if (!allowed) return;
+    $("#reply").html(JSON.stringify(event.data));
+  }
+
+  window.addEventListener("message", replies, false);
+
+  $("button#openme").on("click", function(e) {
+    e.preventDefault();
+    const payload = {
+      action: "open",
+      project_id: proj1,
+      path: "iframe-comm-test.md"
+    };
+    frame.postMessage(payload, target);
+    return false;
+  });
+
+  $("button#calc").on("click", function(e) {
+    e.preventDefault();
+    const payload = {
+      action: "open",
+      project_id: proj1,
+      path: "iframe-comm-nb.ipynb"
+    };
+    frame.postMessage(payload, target);
+    return false;
+  });
+
+  $("button#other").on("click", function(e) {
+    e.preventDefault();
+    const payload = {
+      action: "open",
+      project_id: proj2,
+      path: "iframe-comm-test.md"
+    };
+    frame.postMessage(payload, target);
+    return false;
+  });
+
+  $("button#latex").on("click", function(e) {
+    e.preventDefault();
+    const payload = {
+      action: "open",
+      project_id: proj2,
+      path: "latex/iframe-comm.tex"
+    };
+    frame.postMessage(payload, target);
+    return false;
+  });
+
+  $("button#closeall").on("click", function(e) {
+    e.preventDefault();
+    frame.postMessage({ action: "closeall" }, target);
+    return false;
+  });
+
+
+  $("button#status").on("click", function(e) {
+    e.preventDefault();
+    frame.postMessage({ action: "status" }, target);
+    return false;
+  });
+}
